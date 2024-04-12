cf05833ec902ba544ce196dea87fabc5dc610ce6
Kashvir Sewpersad <kashclk31@gmail.com>
2024-04-12 19:25:49 +0200

    Second commit test and confirmation of successful push

--- Program.cs
+++ Program.cs
@@ -10,12 +10,30 @@ using System.Threading.Tasks;
 namespace ST10257503.PROG6221POEPart1
 {
     internal class Program
-    {
+    {/// <summary>
+    /// Kashvir Sewpersad
+    /// ST10257503
+    /// 
+    /// -ref
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    /// 
+
+        //******************** start of main method and entry to program ***********************// 
+
+        /*
+         The goal with my main method is to have a switch statement which will be used to control the actions based of user input
+         This includes method calls and displaying a table 
+         */
         static void Main(string[] args)
         {
             Console.WriteLine("run test");
             Console.ReadKey();
         }
+
+        //******************* end of main method ******************************************//
+
     }
 }
 
